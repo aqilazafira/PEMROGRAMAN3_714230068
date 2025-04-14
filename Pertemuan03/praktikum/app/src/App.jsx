b3a5c535ddf662a6f57d9c78865750c2cfc53ee0
Aqila Zafira <hartaticpg@gmail.com>
2025-04-14 15:38:58 +0700

    praktikum&tugas

---
+++ Pertemuan03/praktikum/app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+import Dashboard from './pages/Dashboard';
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen gap-4">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
