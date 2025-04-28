ec1517adf8ab735f65e273bc628743c0c770131c
Aqila Zafira <hartaticpg@gmail.com>
2025-04-28 15:45:31 +0700

    praktikum p06

--- Pertemuan05/praktikum/config/cors.go
+++ Pertemuan05/praktikum/config/cors.go
@@ -2,7 +2,8 @@ package config
 
 var allowedOrigins = []string{
 	"http://localhost:3000",
-	"https://indrariksa.github.io",
+	"https://indrariksa.github.io/",
+	"http://localhost:5174/",
 }
 
 func GetAllwedOrigins() []string {
