8fa84082f56078b2854e7f9a5f78034b5320438e
Aqila Zafira <hartaticpg@gmail.com>
2025-06-17 02:41:03 +0700

    bckend

--- Pertemuan11/praktikum/frontend/src/components/atoms/TypographyAtom.jsx
+++
@@ -1,9 +0,0 @@
-import { Typography } from "@material-tailwind/react";
-
-export function TypographyAtom({ children, variant = "paragraph", color = "blue-gray", className = "" }) {
-  return (
-    <Typography variant={variant} color={color} className={className}>
-      {children}
-    </Typography>
-  );
-}
