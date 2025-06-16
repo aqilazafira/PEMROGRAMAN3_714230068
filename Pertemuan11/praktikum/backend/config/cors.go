e8d06314b42fa24279174ab4e6b9bf8f080a837e
Aqila Zafira <hartaticpg@gmail.com>
2025-06-16 15:47:07 +0700

    backend

---
+++ Pertemuan11/praktikum/backend/config/cors.go
@@ -0,0 +1,12 @@
+package config
+
+var allowedOrigins = []string{
+	"http://localhost:3000",
+	"https://indrariksa.github.io/",
+	"http://localhost:5174/",
+	"http://localhost:5173/",
+}
+
+func GetAllwedOrigins() []string {
+	return allowedOrigins
+}
