945458994995694aed0479f17817aff2d251fe29
Aqila Zafira <hartaticpg@gmail.com>
2025-06-30 15:24:26 +0700

    url

--- Pertemuan11/tugas/frontend/src/services/authService.js
+++ Pertemuan11/tugas/frontend/src/services/authService.js
@@ -9,6 +9,6 @@ export const login = async (username, password) => {
   return response.data;
 };
 export const register = async (username, password, role) => {
-  const response = await axios.post("http://127.0.0.1:8088/register", { username, password, role });
+  const response = await axios.post(API_URL_REGISTER, { username, password, role });
   return response.data;
 };
